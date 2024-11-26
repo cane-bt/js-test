52eac7bebf76b17e56287b64e8bd5d87839f83a7
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-11-27 05:49:17 +0900

    パラメタライズドテスト
    https://www.udemy.com/course/ts-webapp-test/learn/lecture/38186112#overview

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,5 +1,14 @@
 import { sum } from "./sum";
 
-it("1+2は3である", () => {
-    expect(sum(1, 2)).toBe(3)
+// it("1+2は3である", () => {
+//     expect(sum(1, 2)).toBe(3)
+// })
+
+// https://jestjs.io/ja/docs/api#testeachtablename-fn-timeout
+it.each`
+    a | b | expected
+    ${1} | ${2} | ${3}
+    ${1} | ${-2} | ${-1}
+`("$aと$bを足すと$expectedになる", ({ a, b, expected }) => {
+    expect(sum(a, b)).toBe(expected)
 })
